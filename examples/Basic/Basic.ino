0419c08aabee34eda917155189f5c576acd8b261
Jacques Bellavance <j-bellavance@videotron.ca>
2017-03-29 05:29:50 -0400

    Add files via upload

---
+++ examples/Basic/Basic.ino
@@ -0,0 +1,16 @@
+#include <CommonBusEncoders.h>
+
+CommonBusEncoders encoders(34, 35, 36, 2);
+
+void setup() {
+  encoders.setDebounce(16);
+  encoders.resetChronoAfter(1000);
+  encoders.addEncoder(1, 2, 22, 1, 100,   0);
+  encoders.addEncoder(2, 2, 23, 1, 200, 300);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int index = encoders.readAll();
+  if (index != 0) Serial.println(index);
+}
